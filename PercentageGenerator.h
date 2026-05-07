83bd3cbd5283898c4d325b8e7f921012fbc1dd61
Peter Holifield <peterholifield@outlook.com>
2026-05-07 18:41:30 -0400

    fixed a bug with initialization

--- PercentageGenerator.h
+++ PercentageGenerator.h
@@ -3,8 +3,8 @@
 class RandomNumberGenerator {
 	std::mt19937 gen;
 public:
-	std::random_device rd;
-	RandomNumberGenerator() { gen.seed(rd()); };
+	
+	RandomNumberGenerator() { std::random_device rd; gen.seed(rd()); };
 	float getFloat(float top) { std::uniform_real_distribution<float> dist(0.0f, top); return dist(gen); }
 	int getInt(int top) { std::uniform_int_distribution<int> dist(0, top); return dist(gen); }
 };
