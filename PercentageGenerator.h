53ac5da6fc29c8bb7f95ae700b6a8a3814225bc8
Peter Holifield <peterholifield@outlook.com>
2026-05-07 17:50:46 -0400

    replaced time with random_device for rng seeding

--- PercentageGenerator.h
+++ PercentageGenerator.h
@@ -3,7 +3,8 @@
 class PercentageGenerator {
 	std::mt19937 gen;
 public:
-	PercentageGenerator() { gen.seed(std::time(nullptr)); };
+	std::random_device rd;
+	PercentageGenerator() { gen.seed(rd()); };
 	float getFloat(float top) { std::uniform_real_distribution<float> dist(0.0f, top); return dist(gen); }
 	int getInt(int top) { std::uniform_int_distribution<int> dist(0, top); return dist(gen); }
 };
